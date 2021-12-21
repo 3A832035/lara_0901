b55bef304af0d92d38903887185b7985ea3a1944
3A832035 <3a832035@gm.student.ncut.edu.tw>
2021-12-22 00:22:55 +0800

    新增資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -24,4 +24,10 @@ class AdminPostsController extends Controller
 
         return view('admin.posts.edit', $data);
     }
+
+    public function store(PostRequest $request)
+    {
+        Post::create($request->all());
+        return redirect()->route('admin.posts.index');
+    }
 }
